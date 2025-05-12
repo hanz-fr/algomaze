2b86153a358c2177a4cfba7da5ad5a9c42a396c3
hanz-fr <raihanf787@gmail.com>
2025-05-12 23:34:55 +0700

    refactor: project modularity

---
+++ include/maze.h
@@ -0,0 +1,7 @@
+#ifndef MAZE_H
+#define MAZE_H
+#include <vector>
+
+std::vector<std::vector<bool>> createMaze();
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include "include/maze.h"
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+
+
+int main()
+{
+    std::vector<std::vector<bool>> maze = createMaze();
+
+    for (const auto& row : maze) {
+        for (bool cell : row) {
+            std::cout << (cell ? char(219) : ' ');
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
