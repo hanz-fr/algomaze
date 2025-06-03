ecfc6a6abf981af338d53ea74d38c6430fea46eb
hanz-fr <raihanf787@gmail.com>
2025-06-04 00:02:18 +0700

    feat: add draw maze region, customizable row and columns

--- src/create_your_own_maze.cpp
+++ src/create_your_own_maze.cpp
@@ -1,5 +1,7 @@
 #include <string>
 
+#include "../include/empty_maze.h"
+
 #include "../imgui/imgui.h"
 #include "../imgui/backends/imgui_impl_sdl2.h"
 #include "../imgui/backends/imgui_impl_opengl3.h"
@@ -36,26 +38,78 @@ int main(int, char **)
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplSDL2_NewFrame();
         ImGui::NewFrame();
+        ImGui::Begin("##");
+        
+        /* MAZE DRAW REGION */
+        const int rows = 25;
+        const int cols = 25;
 
-        ImGui::Begin("Draw your own maze");
-
-        bool checkboxes[25] = {};
-        /* TABLE */
-        if (ImGui::BeginTable("table3", 25))
+        static int row_size_input = 0;
+        static int col_size_input = 0;
+        ImGui::InputInt("Row Size", &row_size_input);
+        ImGui::InputInt("Column Size", &col_size_input);
+        ImGui::Button("Save Maze");
+        
+        ImGui::Text("Draw your own maze!");
+        ImGui::Text("Size: %dx%d", row_size_input, col_size_input);
+        
+        static bool maze[rows][cols] = {false}; // true = wall
+        float cellSize = 15.0f; // seberapa gede kotak2nya
+        for (int i = 0; i < row_size_input; i++)
         {
-            for (int item = 0; item < 25; item++)
+            for (int j = 0; j < col_size_input; j++)
             {
-                ImGui::TableNextColumn();
-                std::string label = "##checkbox" + std::to_string(item);
-                ImGui::Checkbox(label.c_str(), &checkboxes[item]);
+                ImGui::PushID(i * col_size_input + j);
+
+                // Set button color based on wall/path
+                ImGui::PushStyleColor(ImGuiCol_Button, maze[i][j] ? ImVec4(0.2f, 0.2f, 0.2f, 1.0f) : ImVec4(1, 1, 1, 1));
+                ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.4f, 0.4f, 0.4f, 1.0f));
+                ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.2f, 0.2f, 0.2f, 1.0f));
+
+                if (ImGui::Button(" ", ImVec2(cellSize, cellSize)))
+                {
+                    maze[i][j] = !maze[i][j];
+                }
+
+                ImGui::PopStyleColor(3);
+                ImGui::PopID();
+
+                if (j < col_size_input - 1)
+                    ImGui::SameLine();
             }
-            ImGui::EndTable();
         }
 
-        ImGui::Text("Dear ImGui on Windows!");
+        /* TABLE */
+        // ImGui::BeginChild("TableRegion", ImVec2(100,100), true);
+        // const int col = 5;
+        // const int row = 5;
+        // bool checkboxes[col][col] = {};
+        // if (ImGui::BeginTable("table1", col, ImGuiTableFlags_Borders))
+        // {
+        //     for (int i = 0; i < row; i++)
+        //     {
+        //         ImGui::TableNextRow();
+        //         for (int j = 0; j < col; j++)
+        //         {
+        //             ImGui::TableSetColumnIndex(j);
+        //             std::string label = "##checkbox_" + std::to_string(i) + "_" + std::to_string(j);
+        //             ImGui::Checkbox(label.c_str(), &checkboxes[i][j]);
+        //         }
+        //     }
+        //     ImGui::EndTable();
+        // }
+        // ImGui::EndChild();
 
-        static float rotation = 0.0;
-        ImGui::SliderFloat("rotation", &rotation, 0, 10);
+        // if (ImGui::BeginTable("table3", 25))
+        // {
+        //     for (int item = 0; item < 25; item++)
+        //     {
+        //         ImGui::TableNextColumn();
+        //         std::string label = "##checkbox" + std::to_string(item);
+        //         ImGui::Checkbox(label.c_str(), &checkboxes[item]);
+        //     }
+        //     ImGui::EndTable();
+        // }
 
         if (ImGui::Button("Close"))
             show = false;
