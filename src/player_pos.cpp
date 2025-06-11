420632a9b6603822673e1a658c8a32b7564e8c69
nanand.r <rafirenan12@gmail.com>
2025-06-11 21:05:18 +0700

    player move stack

--- src/player_pos.cpp
+++ src/player_pos.cpp
@@ -13,32 +13,37 @@ struct History {
 
 History history;
 
-void push (pair<int, int> data) {
-    // if data is not the same as top-1.data
-    if (history.top == -1 || history.data[history.top-1] != data) {
-        history.top++;
-        history.data[history.top] = data;
-    }
-    // else remove top
-    else {
-        pop();
+void pop () {
+    if (history.top != -1) {
+        history.top--;
     }
 }
 
-void pop () {
-    if (!history.top == -1) {
-        history.top--;
+void push(pair<int, int> data) {
+    // Check if user undo their last move (go back to top - 1 position)
+    if (history.top >= 1 && history.data[history.top - 1] == data) {
+        pop();  // remove the most recent position
+    } else {
+        history.top++;
+        if (history.top < history.data.size()) {
+            history.data[history.top] = data;
+        } else {
+            history.data.push_back(data);
+        }
     }
 }
 
 void showPlayerPos(int row_pos, int col_pos)
 {
     pair<int, int> pos = {row_pos, col_pos};
+    push(pos);
+    
     clearScreen();
 
     cout << "Position: [" << pos.first << "][" << pos.second << "]" << endl;
     cout << "History: ";
     for (int i = history.top; i >= 0; i--) {
-        cout << "[" << history.data[i].first << "][" << history.data[i].second << "]" << endl;
+        cout << "[" << history.data[i].first << "][" << history.data[i].second << "]";
     }
+    cout << endl;
 }
